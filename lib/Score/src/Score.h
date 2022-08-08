7a89f6b894441a0d268ac83917bcd815fbee4825
bvujovic <bv.net@outlook.com>
2022-08-08 22:20:29 +0200

    M Score: M pointWon(), A undoLastPoint(), isMatchOver() M test_main.cpp: A 6 test methods M main.cpp: dbl click - undo/reset, display score on TM1637...

--- lib/Score/src/Score.h
+++ lib/Score/src/Score.h
@@ -6,24 +6,31 @@ enum Side
     Away = 1
 };
 
+// Ping-pong score.
 class Score
 {
 private:
-    int homePoints;
-    int awayPoints;
-    Side serveCurrent;
-    Side serveStarted;
+    int homePoints;           // how many points has the person with controls (this device)
+    int awayPoints;           // how many points has the other person
+    Side lastPointWon;        // who won last point
+    Side serveCurrent;        // who is serving next
+    Side serveStarted = Away; // who served first
+    bool isOver;              // is match over
 
 public:
     Score();
-    void resetScore();
+    void reset();
     void changeServe();
 
     void pointWon(Side side);
-    // void undoLastPoint();
+    void undoLastPoint();
 
     int getHomePoints() { return homePoints; }
     int getAwayPoints() { return awayPoints; }
     Side getServe() { return serveCurrent; }
+    bool isMatchOver() { return isOver; }
+
+private:
+    void calcWhoServesNext();
 
 };
